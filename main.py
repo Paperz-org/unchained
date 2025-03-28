c8a658011c84c23a6adbbccd5a7ca5dd76074e48
pbrocharPaper <paul.brochard@papernest.com>
2025-03-29 00:15:15 +0100

    Add dependency injection

--- main.py
+++ main.py
@@ -1,22 +1,54 @@
+import inspect
+from typing import Annotated
+
+from fast_depends import Depends
+
 from models import User
 from unchained import Unchained
 
 # Create API instance and set up Django
 app = Unchained()
 
+
+def dependency() -> str:
+    return "my dependency"
+
+
+def toto(func):
+    signature = inspect.signature(func)
+
+    def wrapper(*args, **kwargs):
+        # Filter out parameters with Annotated type
+        filtered_params = {}
+        for name, param in signature.parameters.items():
+            if param.annotation is not inspect.Parameter.empty:
+                if getattr(param.annotation, "__origin__", None) is not Annotated:
+                    filtered_params[name] = param
+            else:
+                filtered_params[name] = param
+
+        # Create new signature without Annotated parameters
+        new_signature = signature.replace(parameters=filtered_params)
+
+        # Apply the new signature to the wrapped function
+        wrapper.__signature__ = new_signature
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
 # Define your endpoints
+
+
 @app.get("/hello")
-def hello(request, name: str = "World"):
-    return {"message": f"Hello {name}!"}
+def hello(request, a: Annotated[str, Depends(dependency)]):
+    return {"message": f"Hello {a}!"}
 
 
-@app.get("/add")
-def add(request, a: int, b: int):
-    return {"result": a + b}
+@app.get("/add/{a}/{b}")
+def add(request, a: int, b: int, c: Annotated[str, Depends(dependency)]):
+    return {"result": a + b, "c": c}
 
-
-# @app.get("/users")
-# def get_users(request):
-#     return {"users": list(User.objects.all())}
 
 app.crud(User)
