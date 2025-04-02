4a966d67a33b5550119adea16b5f9e8e65c2aac7
pbrocharPaper <paul.brochard@papernest.com>
2025-04-02 10:00:06 +0200

    Refacto meta and add router management

--- main.py
+++ main.py
@@ -5,17 +5,16 @@ from pydantic import BaseModel, ConfigDict
 from admin import ProductAdmin, UserAdmin
 from models import Product, User
 from unchained import Depends, Request, Unchained
-from unchained.dependencies.header import Header
-from unchained.settings import UnchainedSettings, settings
-
-
-
+from unchained.settings import UnchainedSettings
+from test_router import router
 
 class Headers(UnchainedSettings):
     x_api_key: str | None = None
 
 
 app = Unchained()
+
+app.add_router("/router", router)
 
 app.admin.register(User, UserAdmin)
 app.admin.register(Product, ProductAdmin)
@@ -55,7 +54,6 @@ def hello(a: str, b: Annotated[str, Depends(dep)]):
 @app.get("/ahello/{a}")
 async def ahello(a: str, b: Annotated[str, Depends(dep)]):
     return {"message": f"ASYNC Hello {a} {b} !"}
-
 
 
 app.crud(User, operations="CRUD")
