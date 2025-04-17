ea744942c44ba038b63b621f1511f03167e83fa2
Maxime de Pachtere <maxime.depachtere@gmail.com>
2025-04-17 12:47:20 +0200

    add swagger customisation

--- src/unchained/unchained.py
+++ src/unchained/unchained.py
@@ -6,19 +6,17 @@ from typing import TYPE_CHECKING, Any, Callable
 from django.db.models import QuerySet
 from django.urls import path
 
-
+from unchained import context
 from unchained.admin import UnchainedAdmin
 from unchained.base import BaseUnchained
+from unchained.docs.swagger import UnchainedSwagger
 from unchained.lifespan import Lifespan
 from unchained.meta import UnchainedMeta, URLPatterns
 from unchained.settings.base import UnchainedSettings
 from unchained.states import BaseState
-from unchained import context
-
 
 if TYPE_CHECKING:
     from .models.base import BaseModel
-
 
 class Unchained(BaseUnchained, metaclass=UnchainedMeta):
     APP_NAME = "unchained.app"
@@ -37,9 +35,7 @@ class Unchained(BaseUnchained, metaclass=UnchainedMeta):
         self.state = state or BaseState()
 
         self._lifespan = self._wrap_lifespan(lifespan) if lifespan else None
-
-        # Call parent init
-        super().__init__(**kwargs)
+        super().__init__(**kwargs, docs=UnchainedSwagger())
         context.app.set(self)
 
     def lifespan(self, func: Callable):
